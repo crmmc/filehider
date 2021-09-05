674fec5c5df6f6e45f5856e4a2798d81b996d819
crmmc <1879312087@qq.com>
2021-09-05 19:36:08 +0800

    add some function
    big update,see read me

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # filehider
- A program to hide your file
+The FileHider
+
+A simple tool to hide your file
+
+Usage: 
+    filehider [Option] [File1] [File2] [File...]
+
+The Program will automatic encode/decode files
+
+Options:
+    -n use for disable sha1 check,but it can only be used when decoding file
+    -f Remove the "_" before the output file name
+    -s Turn on the auto rename switch,the output file name can cheat mechine prefectly
+    --ext=[extname] can reset the output file suffix (default: mp4)
+Example:
+    filehider C:/pagefile.sys D:/test.zip --ext=mov -n -s 
--- main.go
+++ main.go
@@ -7,14 +7,23 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"path/filepath"
+	"strings"
 	"unsafe"
 )
 
+/*
+错误返回码定义：
+正常：0
+命令行参数错误：1
+输入文件打开失败：2
+输出文件打开失败：3
+*/
+
 var disablesha1 bool           //SHA1校验开关
+var enablerename bool          //乱序文件名格式开关
 var fileextname string = "mp4" //输出的加密文件的后缀名
 var cfilename string = "_"     //还原的文件的文件名前缀，防止源文件存在导致覆盖写入
-var buffersize int = 8192      //读入缓存空间大小
+var buffersize int = 4096      //读入缓存空间大小
 var fileheadle []byte = []byte{
 	0x00, 0x00, 0x00, 0x18, 0x66, 0x74, 0x79, 0x70, 0x6D, 0x70, 0x34, 0x32, 0x00, 0x00, 0x00, 0x00,
 	0x6D, 0x70, 0x34, 0x32, 0x6D, 0x70, 0x34, 0x31, 0x00, 0x04, 0x38, 0xDC, 0x6D, 0x6F, 0x6F, 0x76,
@@ -22,28 +31,104 @@ var fileheadle []byte = []byte{
 	0xDD, 0x53, 0x54, 0x5C, 0x00, 0x01, 0x5F, 0x90, 0x01, 0xD5, 0x94, 0x3F, 0xDD, 0x53} //用于校验和伪装的文件头
 
 func main() {
-	argv := os.Args   //系统传入的命令行参数列表
+	argv := os.Args   //系统传入的命令行参数
 	argc := len(argv) //系统传入的命令行参数个数
-	if argc == 3 {
-		if argv[2] == "-n" {
-			disablesha1 = true //-n开关被开启
+	if argc == 1 {
+		help(argv[1])
+		os.Exit(0)
+	}
+	var fileinputs []string
+	for i := 1; i < argc; i++ {
+		if argv[i] == "-h" {
+			help(argv[0])
+			os.Exit(0)
 		}
-	} else if argc != 2 {
-		help(argv[0])
-		os.Exit(1)
+		if argv[i] == "-n" {
+			disablesha1 = true //-n开关被开启
+			continue
+		}
+		if argv[i] == "-s" {
+			enablerename = true //-s开关被开启
+			continue
+		}
+		if argv[i] == "-f" {
+			cfilename = "" //-f开关被开启
+			continue
+		}
+		if len(argv[i]) > 6 {
+			if argv[i][0:6] == "--ext=" {
+				fileextname = argv[i][6:] //设定输出后缀名
+				fmt.Println("SET EXT: ." + fileextname)
+				continue
+			}
+		}
+		fileinputs = append(fileinputs, argv[i]) //拼接字符串
 	}
-	inf, inferr := os.Open(argv[1]) //打开输入文件
+	errfs := []string{}
+	for i := 0; i < len(fileinputs); i++ {
+		ret := process(fileinputs[i])
+		if ret == 3 {
+			fmt.Println("Open Output File " + fileinputs[i] + " Failed!")
+			errfs = append(errfs, fileinputs[i])
+		} else if ret == 2 {
+			fmt.Println("Open Input File " + fileinputs[i] + " Failed!")
+			errfs = append(errfs, fileinputs[i])
+		} else if ret == 1 {
+			help(argv[0])
+			os.Exit(1)
+		} else if ret != 0 {
+			fmt.Printf("Doing %s Got Different Return: ", fileinputs[i])
+			errfs = append(errfs, fileinputs[i])
+			fmt.Println(ret)
+		}
+	}
+	if len(errfs) == 0 {
+		fmt.Println("All Tasks Successful!")
+		os.Exit(0)
+	} else {
+		fmt.Println("Error Files List:")
+		for i := 0; i < len(errfs); i++ {
+			fmt.Println(errfs[i])
+		}
+		fmt.Println("END")
+	}
+}
+
+func process(inputfilename string) int {
+	inf, inferr := os.Open(inputfilename) //打开输入文件
 	if inferr != nil {
-		fmt.Println("Open Input File " + argv[1] + " Failed!")
+		return 2
 	}
-	var outfn string = ""                 //输出文件
-	orfp, orfn := filepath.Split(argv[1]) //分割输入文件的路径和文件名
+	var outfn string = "" //输出文件
+	fmt.Println("Input: " + inputfilename)
+	//分割输入文件的路径和文件名
+	var orfn, orfp string //path模块有毛病啊，为什么返回值全是空字符串啊，害的我自己写
+	if strings.Contains(inputfilename, "\\") {
+		a2 := strings.Split(inputfilename, "\\")
+		orfn = a2[(len(a2) - 1)]
+		a2 = a2[:(len(a2) - 1)]
+		orfp = a2[0] + "\\"
+		for i := 1; i < len(a2); i++ {
+			orfp = orfp + a2[i] + "\\"
+		}
+	} else if strings.Contains(inputfilename, "/") {
+		a2 := strings.Split(inputfilename, "/")
+		orfn = a2[(len(a2) - 1)]
+		a2 = a2[:(len(a2) - 1)]
+		orfp = a2[0] + "/"
+		for i := 1; i < len(a2); i++ {
+			orfp = orfp + a2[i] + "/"
+		}
+	} else {
+		orfp = "./"
+		orfn = inputfilename
+	}
 	//读识别用文件头
 	readsize1 := len(fileheadle)
 	infheadle := make([]byte, readsize1)
 	inf.Read(infheadle)
-	//
 	sha := sha1.New() //sha1检验对象
+	shatext := ""     //储存sha1校验值字符串
 	//分析是否符合文件头
 	if analyze(fileheadle, infheadle) {
 		//符合文件头，判断为已被加密的文件
@@ -52,9 +137,6 @@ func main() {
 			fmt.Println("SHA1 Check: Disable")
 		} else {
 			fmt.Println("SHA1 Check: Enable")
-		}
-		if orfp == "" {
-			orfp = "./" //防止出现空文件路径
 		}
 		// 读储存的文件名变量大小
 		nbl := make([]byte, 4)
@@ -68,16 +150,12 @@ func main() {
 		outfn = orfp + rn //组合输出文件路径
 		outf, oerr := os.Create(outfn)
 		if oerr != nil {
-			fmt.Println("Open Output File " + outfn + " Failed")
-			os.Exit(4)
-		}
-		if disablesha1 {
-			fmt.Println("SHA1 Check: Disable")
+			return (3)
 		}
 		rbshadata := make([]byte, 20) //储存从文件读到的sha1
 		inf.Read(rbshadata)           //读sha1值
 		buf := make([]byte, buffersize)
-		for {
+		for { //没有while true，go里这样实现
 			count, rerr := inf.Read(buf)
 			if rerr == io.EOF {
 				break
@@ -92,30 +170,38 @@ func main() {
 		if !disablesha1 {
 			rshadata := sha.Sum(nil) //计算sha1
 			if analyze(rshadata, rbshadata) {
-				fmt.Println("SHA1 Check: Okey")
+				fmt.Println("SHA1 Check: Pass")
 			} else {
 				fmt.Println("SHA1 Check: File may broken")
 			}
 		} else {
 			fmt.Println("SHA1 Check: Please check file sha1 by yourself")
 		}
+		enablerename = false
 	} else {
-		inf.Seek(0, io.SeekStart)           //是个未加密的文件，移动文件指针到文件开头
-		outfn = argv[1] + "." + fileextname //生成文件名
+		inf.Seek(0, io.SeekStart) //是个未加密的文件，移动文件指针到文件开头
+		if enablerename {
+			fmt.Println("AutoRename Mode ON")
+			outfn = fmt.Sprintf("%x", sha1.New().Sum(str2bytes(inputfilename))) + "." + fileextname //利用sha1生成唯一的文件名
+		} else {
+			outfn = inputfilename + "." + fileextname //生成文件名
+		}
 		fmt.Println("Mode: Encode")
-		fmt.Println("SHA1 Check: Enable")
 		if disablesha1 {
-			fmt.Println("SHA1 Check: ignore \"-b\" switch")
+			fmt.Println("SHA1 Check: Enable")
+			fmt.Println("SHA1 Check: ignore \"-n\" switch")
 		}
 		outf, oerr := os.Create(outfn) //若文件存在则覆盖写入，不存在就创建
 		if oerr != nil {
-			fmt.Println("Open Output File " + outfn + " Failed")
-			os.Exit(2)
+			return 3
 		}
 		outf.Write(fileheadle) //写文件头
 		//转换并写入文件名数据长度和文件名数据
+		//orfn正常
 		a1 := str2bytes(orfn)
+		orfnc := orfn + " " //防止到后面输出全是乱码和被编译器优化导致内存指针指向orfn，只能随便拼接一个无用字符
 		outf.Write(IntToBytes(len(a1)))
+		//到这里orfn变量的内容会被破坏，输出是乱码，估计和这个函数直接的内存操作有关
 		outf.Write(encode(a1))
 		wshaseek, _ := outf.Seek(0, io.SeekCurrent) //用于储存sha1写入位置的文件指针
 		outf.Write(make([]byte, 20))                //先写个空值占位
@@ -126,23 +212,43 @@ func main() {
 				break
 			}
 			wrb := buf[0:count]
-			sha.Write(wrb)
-			outf.Write(encode(wrb))
+			sha.Write(wrb)          //sha1数据读入
+			outf.Write(encode(wrb)) //数据输出到文件
 		}
 		shadata := sha.Sum(nil)
-		outf.Seek(wshaseek, io.SeekStart)     //回到sha1数据的位置
-		shatext := fmt.Sprintf("%x", shadata) //sha1数据转换成字符串好展示
+		outf.Seek(wshaseek, io.SeekStart)    //回到sha1数据的位置
+		shatext = fmt.Sprintf("%x", shadata) //sha1数据转换成字符串好展示
 		fmt.Println("SHA1 Check: " + shatext)
 		outf.Write(shadata) //写sha1值到文件
 		outf.Close()
+		//产生文件报告
+		if enablerename {
+			report, lerr := os.Create(outfn + ".report.txt")
+			if lerr == nil {
+				report.WriteString("原始文件名:" + orfnc + "\n更改后文件名:" + outfn + "\n原始文件sha1:" + shatext)
+				report.Close()
+			} else {
+				fmt.Println("Waring! Report General Failed!")
+				report.Close()
+			}
+		}
 	}
 	inf.Close()
-	fmt.Println("Done")
+	fmt.Println("Ouput: " + outfn)
+	return 0
 }
 
 //帮助
 func help(argv0 string) {
-	fmt.Println("Usage: " + argv0 + " [FilePath] [Option]\nOption \"-n\" can only be used when decoding file")
+	fmt.Println("The FileHider")
+	fmt.Println("A simple tool to hide your file")
+	fmt.Println("Usage: " + argv0 + " [Option] [File1] [File2] [File...]")
+	fmt.Println("Program will automatic encode/decode files")
+	fmt.Println("Options:\n\t\"-n\" use for disable sha1 check,but it can only be used when decoding file")
+	fmt.Println("\t\"-f\" Remove the \"_\" before the output file name")
+	fmt.Println("\t\"-s\" Turn on the auto rename switch,the output file name can cheat mechine prefectly")
+	fmt.Println("\t\"--ext=\" can reset the output file suffix (default: mp4)")
+	fmt.Println("Example:\n\t" + argv0 + " C:/pagefile.sys D:/test.zip --ext=mov -n -s ")
 }
 
 //二进制数据逐字节比较
@@ -153,6 +259,14 @@ func analyze(bigbyte, smallbyte []byte) bool {
 		}
 	}
 	return true
+}
+
+//简单的取反加密函数
+func encode(inb []byte) []byte {
+	for i := 0; i < len(inb); i++ {
+		inb[i] = ^inb[i]
+	}
+	return inb
 }
 
 //整形转换成字节
@@ -172,14 +286,6 @@ func BytesToInt(b []byte) int {
 	var x int32
 	binary.Read(bytesBuffer, binary.BigEndian, &x)
 	return int(x)
-}
-
-//简单的取反加密函数
-func encode(inb []byte) []byte {
-	for i := 0; i < len(inb); i++ {
-		inb[i] = ^inb[i]
-	}
-	return inb
 }
 
 //高效的字符串转字节组
